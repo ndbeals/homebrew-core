01791185a1cbcbc540db5a4ff5d3a3a73f029198
Michael Cho <michael@michaelcho.dev>
2024-10-29 11:48:39 -0400

    libzim: revision bump to migrate to `icu4c@76`

--- Formula/lib/libzim.rb
+++ Formula/lib/libzim.rb
@@ -4,7 +4,7 @@ class Libzim < Formula
   url "https://github.com/openzim/libzim/archive/refs/tags/9.2.3.tar.gz"
   sha256 "7c6e7fcaf5bc82447edb12c6c573779af6d77b3b79227da57586e81c4e13f1bf"
   license "GPL-2.0-or-later"
-  revision 1
+  revision 2
 
   bottle do
     sha256 cellar: :any, arm64_sequoia: "d339d5a5890ded1fb8757e081dd2e4d528e3e67e40076b10a0dd15cc82d56723"
@@ -19,7 +19,7 @@ class Libzim < Formula
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
 
-  depends_on "icu4c@75"
+  depends_on "icu4c@76"
   depends_on "xapian"
   depends_on "xz"
   depends_on "zstd"
